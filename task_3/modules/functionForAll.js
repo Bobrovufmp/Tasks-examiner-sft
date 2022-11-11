d7282023aa4aa9bd7bdb3e2636b532b7490adc2e
Tsimafei <102069584+Bobrovufmp@users.noreply.github.com>
2022-11-11 15:49:44 +0100

    Add files via upload

---
+++ task_3/modules/functionForAll.js
@@ -0,0 +1,183 @@
+export function usedForI1(el, i) {
+  if (el == "D") {
+    i = "3";
+    return i;
+  } else {
+    return i;
+  }
+}
+
+export function usedForI2(el, i) {
+  if (el == "D") {
+    i = "6";
+    return i;
+  }
+  if (el == "R") {
+    i = "3";
+    return i;
+  } else {
+    return i;
+  }
+}
+
+export function usedForI3(el, i) {
+  if (el == "D") {
+    i = "7";
+    return i;
+  }
+  if (el == "R") {
+    i = "4";
+    return i;
+  }
+  if (el == "U") {
+    i = "1";
+    return i;
+  }
+  if (el == "L") {
+    i = "2";
+    return i;
+  }
+}
+
+export function usedForI4(el, i) {
+  if (el == "D") {
+    i = "8";
+    return i;
+  }
+  if (el == "L") {
+    i = "3";
+    return i;
+  } else {
+    return i;
+  }
+}
+
+export function usedForI5(el, i) {
+  if (el == "R") {
+    i = "6";
+    return i;
+  } else {
+    return i;
+  }
+}
+
+export function usedForI6(el, i) {
+  if (el == "D") {
+    i = "A";
+    return i;
+  }
+  if (el == "R") {
+    i = "7";
+    return i;
+  }
+  if (el == "U") {
+    i = "2";
+    return i;
+  }
+  if (el == "L") {
+    i = "5";
+    return i;
+  }
+}
+
+export function usedForI7(el, i) {
+  if (el == "D") {
+    i = "B";
+    return i;
+  }
+  if (el == "R") {
+    i = "8";
+    return i;
+  }
+  if (el == "U") {
+    i = "3";
+    return i;
+  }
+  if (el == "L") {
+    i = "6";
+    return i;
+  }
+}
+
+export function usedForI8(el, i) {
+  if (el == "D") {
+    i = "C";
+    return i;
+  }
+  if (el == "R") {
+    i = "9";
+    return i;
+  }
+  if (el == "U") {
+    i = "4";
+    return i;
+  }
+  if (el == "L") {
+    i = "7";
+    return i;
+  }
+}
+
+export function usedForI9(el, i) {
+  if (el == "L") {
+    i = "8";
+    return i;
+  } else {
+    return i;
+  }
+}
+
+export function usedForIA(el, i) {
+  if (el == "U") {
+    i = "6";
+    return i;
+  }
+  if (el == "R") {
+    i = "B";
+    return i;
+  } else {
+    return i;
+  }
+}
+
+export function usedForIB(el, i) {
+  if (el == "D") {
+    i = "D";
+    return i;
+  }
+  if (el == "R") {
+    i = "C";
+    return i;
+  }
+  if (el == "U") {
+    i = "7";
+    return i;
+  }
+  if (el == "L") {
+    i = "A";
+    return i;
+  }
+}
+
+export function usedForIC(el, i) {
+  if (el == "U") {
+    i = "8";
+    return i;
+  }
+  if (el == "L") {
+    i = "B";
+    return i;
+  } else {
+    return i;
+  }
+}
+
+export function usedForID(el, i) {
+  if (el == "U") {
+    i = "B";
+    return i;
+  } else {
+    return i;
+  }
+}
+
